e40c00b1690ae526efa4d94388b9e49d2244a69d
Siddiqov <smdplayer1999@gmail.com>
2021-07-21 18:11:41 +0500

    add bottom_menu.xml and CheckPassFragment.kt

---
+++ app/src/main/java/ru/smd/passnumber/CheckPassFragment.kt
@@ -0,0 +1,8 @@
+package ru.smd.passnumber
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Siddikov Mukhriddin on 7/21/21
+ */
+class CheckPassFragment :Fragment(R.layout.fragment_check_pass)
--- app/src/main/java/ru/smd/passnumber/MainActivity.kt
+++ app/src/main/java/ru/smd/passnumber/MainActivity.kt
@@ -2,11 +2,33 @@ package ru.smd.passnumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.bottom_menu.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handleBottomClick()
+    }
+
+    fun handleBottomClick() {
+        btnBottom1.isSelected = true
+        supportFragmentManager.beginTransaction().replace(R.id.mainContainer,CheckPassFragment()).commit()
+        btnBottom1.setOnClickListener {
+            btnBottom1.isSelected = true
+            btnBottom2.isSelected = false
+            btnBottom3.isSelected = false
+        }
+        btnBottom2.setOnClickListener {
+            btnBottom1.isSelected = false
+            btnBottom2.isSelected = true
+            btnBottom3.isSelected = false
+        }
+        btnBottom3.setOnClickListener {
+            btnBottom1.isSelected = false
+            btnBottom2.isSelected = false
+            btnBottom3.isSelected = true
+        }
 
     }
 }
